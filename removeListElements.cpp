d836f0a30bb75232f8ab9afec82db99b22dd34a2
dev082000 <devanshsingh.chandel@gmail.com>
2024-04-14 10:56:03 +0530

    Remove List Elements
    Given the head of a linked list and an integer val, remove all the nodes of the linked list that has Node.val == val, and return the new head.

---
+++ removeListElements.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    ListNode* removeElements(ListNode* head, int val) {
+        if(head==NULL)
+        return head;
+        while(head && head->val==val)
+        {
+            head=head->next;
+        }
+        ListNode* curr=head;
+while(curr && curr->next)
+        {
+            if(curr->next->val==val)
+            curr->next=curr->next->next;
+            else
+            curr=curr->next;
+        }
+        return head;
+       
+    }
+};
