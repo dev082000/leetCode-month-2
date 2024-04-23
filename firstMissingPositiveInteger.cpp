9cc517241c42b793cbaf74a79ba9e5fd8b83e252
dev082000 <devanshsingh.chandel@gmail.com>
2024-04-23 21:28:43 +0530

    FInd the first missing positive integer
    Given an unsorted integer array nums. Return the smallest positive integer that is not present in nums.
    
    You must implement an algorithm that runs in O(n) time and uses O(1) auxiliary space.

---
+++ firstMissingPositiveInteger.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int firstMissingPositive(vector<int>& nums) {
+       sort(nums.begin(),nums.end());
+        vector<int>:: iterator ip;
+        int size=nums.size();
+        ip=unique(nums.begin(),nums.begin()+size);
+       nums.resize(distance(nums.begin(),ip));
+        size=nums.size();
+       int number=1;
+       for(int i=0;i<size;i++) 
+       {
+        if(nums[i]>0)
+        {
+             if(nums[i]!=number)
+          break;
+          number++;
+       }
+    }
+     
+       return number;
+    }
+};
